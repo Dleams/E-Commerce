65033362b76acc1df4518064d81aa4acc02d2911
Dleams <sumbaganl@gmail.com>
2025-04-08 00:17:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,9 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['veg_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Market</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <style>
-   /* Typography */
 body {
   font-family: 'Poppins', sans-serif; /* Change font to Poppins */
   margin: 0;
